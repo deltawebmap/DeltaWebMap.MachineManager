f83daac84f4b81c46d1f7cf39b2dc30e77f377cc
RomanPort <34401219+Roman-Port@users.noreply.github.com>
2020-11-03 21:13:57 -0600

    Fixed a bug that caused Apache2 to fail to load managed sites

--- DeltaWebMap.MachineManager/Framework/Tools/CLITool.cs
+++ DeltaWebMap.MachineManager/Framework/Tools/CLITool.cs
@@ -21,8 +21,45 @@ namespace DeltaWebMap.MachineManager.Framework.Tools
                 RedirectStandardOutput = true
             });
 
-            //Dispatch
-            while(!p.StandardOutput.EndOfStream)
+            return BaseRunProcess(p, logger);
+        }
+
+        public static int RunTerminalCommand(string cmd, IManagerCommandLogger logger, string logTopic, string logMessage)
+        {
+            //Start logging session
+            logger?.LogCLIBegin(logTopic, logMessage);
+
+            //Start process
+            Process p;
+            if(Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                //Linux
+                p = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = cmd,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                });
+            } else
+            {
+                //Windows
+                p = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C " + cmd,
+                    RedirectStandardOutput = true
+                });
+            }
+
+            return BaseRunProcess(p, logger);
+        }
+
+        private static int BaseRunProcess(Process p, IManagerCommandLogger logger)
+        {
+            //Compute
+            while (!p.StandardOutput.EndOfStream)
             {
                 //Get line
                 string line = p.StandardOutput.ReadLine();
@@ -30,8 +67,11 @@ namespace DeltaWebMap.MachineManager.Framework.Tools
             }
 
             //Wait for end
-            logger?.LogCLIOutput("Output stream closed. Waiting for process to exit...");
-            p.WaitForExit();
+            if (!p.HasExited)
+            {
+                logger?.LogCLIOutput("Output stream closed. Waiting for process to exit...");
+                p.WaitForExit();
+            }
 
             //Log
             logger?.LogCLICompletion(p.ExitCode);
